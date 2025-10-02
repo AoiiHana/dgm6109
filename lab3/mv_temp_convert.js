04f89ae3def436c7eb74b8ed52f0cd9903a646aa
JM Velez <jmvelez1196a@gmail.com>
2025-10-02 16:54:04 -0700

    Added comments + dummied-out formula

--- lab3/mv_temp_convert.js
+++ lab3/mv_temp_convert.js
@@ -2,12 +2,31 @@
 
 document.getElementById("submit").addEventListener("click", function(){
     let fahrenheit = document.getElementById("inputF").value; //this is the average for October of last year in Medellin, Colombia, according to TuTiempo.net
+    let conversionType = document.getElementById("unitChoice").value
     let celsius = (fahrenheit - 32) * 5 / 9 
     let kelvin = (Number(fahrenheit) + 459.67) * 5 / 9
 
     output("User Temperature(°F): " + fahrenheit)
-    output("User Temperature(°C): " + celsius)
-    output("User Temperature(K): " + kelvin)
+    if (conversionType == "c"){
+        output("User Temperature(°C): " + celsius)
+    }
+    else{
+        output("User Temperature(K): " + kelvin)
+    }
+    /* 
+        if (conversionType == "c"){
+            output("User Temperature(°C): " + celsius)
+        }
+            
+        if (conversionType == "k"){
+            output("User Temperature(K): " + kelvin)
+        }
+        
+        i honestly had to go back and write the "two if statements" version in after committing this to github because i've already become really used to writing "if-else" in my previous attempts at coding
+        in general i prefer "if-else" because IMO it's more elegant and efficient(?) than discrete if-statements
+        honestly my general ethos of code is very KISS (keep it simple, silly): 
+        whatever is the most lightweight, efficient, streamlined way of doing it that I can come up with, that's how I wanna do it
+    */
 
 });
 
