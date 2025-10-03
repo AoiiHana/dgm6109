66eb5bccc69b550c618bbeacfd64cf1e307d1dd1
JM Velez <jmvelez1196a@gmail.com>
2025-10-02 17:13:44 -0700

    Add files via upload

--- lab3/mv_temp_convert.js
+++ lab3/mv_temp_convert.js
@@ -13,6 +13,20 @@ document.getElementById("submit").addEventListener("click", function(){
     else{
         output("User Temperature(K): " + kelvin)
     }
+    /* 
+        if (conversionType == "c"){
+            output("User Temperature(°C): " + celsius)
+        }
+            
+        if (conversionType == "k"){
+            output("User Temperature(K): " + kelvin)
+        }
+        
+        i honestly had to go back and write the "two if statements" version in after committing this to github because i've already become really used to writing "if-else" in my previous attempts at coding
+        in general i prefer "if-else" because IMO it's more elegant and efficient(?) than discrete if-statements
+        honestly my general ethos of code is very KISS (keep it simple, silly): 
+        whatever is the most lightweight, efficient, streamlined way of doing it that I can come up with, that's how I wanna do it
+    */
 
 });
 
