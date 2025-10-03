edf80b353a8c508a3f7040834ff4a47b0d5257c4
JM Velez <jmvelez1196a@gmail.com>
2025-10-02 18:39:22 -0700

    deleted superfluous comment from older lab

--- lab3/mv_temp_convert.js
+++ lab3/mv_temp_convert.js
@@ -1,7 +1,7 @@
 "use strict"
 
 document.getElementById("submit").addEventListener("click", function(){
-    let fahrenheit = document.getElementById("inputF").value; //this is the average for October of last year in Medellin, Colombia, according to TuTiempo.net
+    let fahrenheit = document.getElementById("inputF").value; 
     let conversionType = document.getElementById("unitChoice").value
     let celsius = (fahrenheit - 32) * 5 / 9 
     let kelvin = (Number(fahrenheit) + 459.67) * 5 / 9
